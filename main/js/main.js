8ac43767f2b50956e2e4e7d7efcc271e596563d6
Rammina <49326578+Rammina@users.noreply.github.com>
2019-06-15 20:50:47 -0600

    Changed table colors and animated trash bin

--- main/js/main.js
+++ main/js/main.js
@@ -2,220 +2,250 @@
 
 // Screen loader
 
-/*
 setTimeout(function () {
-	if (document.readyState === 'loading') {  // Loading hasn't finished yet
-	  document.addEventListener('DOMContentLoaded', function () {
-		document.querySelector(".loader-container").classList.add("no-display");
-  
-	  });
-	} //Loading has already finished
-	else if(document.readyState === "complete" || document.readyState === "interactive" || document.readyState === "loaded"){
-	  console.log(" HTML loaded");
-	  document.querySelector(".loader-container").classList.add("no-display"); //Get rid of the loader
-	}
-  }, 600);
-  */
-  // Checking SVG support
-  
-  var svgSupport = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
-  
-  if (!(svgSupport)) {
-  
-	  let allSvg = document.getElementsByTagName("svg");
-  
-	  for (let svg of allSvg) {
-		  svg.classList.add("no-display");
-		  svg.classList.add("hide-svg");
-	  }
-  
-	  let allSvgFallback = document.querySelectorAll(".svg-fallback");
-  
-	  for (let png of allSvgFallback) {
-		  png.classList.remove("no-display");
-		  png.classList.remove("hide-png");
-	  }
-  
-  
-  
+  if (document.readyState === 'loading') {  // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector(".loader-container").classList.add("no-display");
+
+    });
+  } //Loading has already finished
+  else if(document.readyState === "complete" || document.readyState === "interactive" || document.readyState === "loaded"){
+    console.log(" HTML loaded");
+    document.querySelector(".loader-container").classList.add("no-display"); //Get rid of the loader
   }
+}, 600);
 
+// Checking SVG support
 
+var svgSupport = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
 
-var general = {
-	main: document.querySelector(".main-content")
-}
-var helper = { 
-	openModal(modal){
-		modal.classList.add("show");
-	},
-	closeModal(modal){
-		modal.classList.remove("show");	
+if (!(svgSupport)) {
+
+	let allSvg = document.getElementsByTagName("svg");
+
+	for (let svg of allSvg) {
+		svg.classList.add("no-display");
+		svg.classList.add("hide-svg");
 	}
-};
+
+	let allSvgFallback = document.querySelectorAll(".svg-fallback");
+
+	for (let png of allSvgFallback) {
+		png.classList.remove("no-display");
+		png.classList.remove("hide-png");
+	}
 
 
-var bookList = { 
-	addButton: document.getElementById("add-book"),
-	table: document.getElementById("books-list-1"),
-	tableBody: document.querySelector(".books-list-body"),
-	tableRows: document.getElementsByClassName("books-item"),
-	readIcons: document.querySelectorAll(".books-read-icon"),
-	deleteIcons: document.querySelectorAll(".books-delete-container"),
-	library: [],
-	Book: function(title, author, pages, read){
-		this.title = title;
-		this.author = author;
-		this.pages = pages;
-		this.read = read;
-	},
-	addBook(title, author, pages, read){
-		// Add the book to the array
-		let newBook = new bookList.Book(title, author, pages, read);
-		bookList.library.push(newBook);
 
-		// At the book to the HTML document
-			let item = document.createElement("tr");
-			item.classList.add("books-row");
-			item.classList.add("books-item");
-			item.insertAdjacentHTML("beforeend", `
-                    <td class="books-number"></td>
-                    <td class="books-title">${title}</td>
-                    <td class="books-author">${author}</td>
-                    <td class="books-pages">${pages}</td>
-                    `);
-			if(read === true) {
-				item.insertAdjacentHTML("beforeend", `
-					<img class="books-read-icon not-read" src="main/images/x-icon.png" alt="X icon">`);
-				
-			}
-			else if(read === false) {
-				item.insertAdjacentHTML("beforeend", `
-					<img class="books-read-icon read" src="main/images/check.png" alt="check icon">`);
-			}
-            item.insertAdjacentHTML("beforeend", `
-            	<td class="books-delete">
-                        <svg width="80" height="80" class="delete-svg">
-                            <!-- <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
-                            <image class="delete-image" href="https://image.flaticon.com/icons/svg/121/121113.svg" src="main/images/delete.svg" width="90" height="90" alt="Trash bin"/>
-                        </svg>
-                        <img class="hide-png no-display svg-fallback delete-png" src="main/images/delete.png" alt="Trash bin">
-                        `);
-			bookList.tableBody.appendChild(item);
-			// counter++;
-
-	},
-	render(library){
-		// let counter = 1;
-		for (let book of library){
-
-			let item = document.createElement("tr");
-			item.classList.add("books-row");
-			item.classList.add("books-item");
-			item.insertAdjacentHTML("beforeend", `
-                    <td class="books-number"></td>
-                    <td class="books-title">${book.title}</td>
-                    <td class="books-author">${book.author}</td>
-                    <td class="books-pages">${book.pages}</td>
-                    `);
-			if(book.read === true) {
-				item.insertAdjacentHTML("beforeend", `
-					<img class="books-read-icon not-read" src="main/images/x-icon.png" alt="X icon">`);
-				
-			}
-			else if(book.read === false) {
-				item.insertAdjacentHTML("beforeend", `
-					<img class="books-read-icon read" src="main/images/check.png" alt="check icon">`);
-			}
-            item.insertAdjacentHTML("beforeend", `
-            	<td class="books-delete">
-                        <svg width="80" height="80" class="delete-svg">
-                            <!-- <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
-                            <image class="delete-image" href="https://image.flaticon.com/icons/svg/121/121113.svg" src="main/images/delete.svg" width="90" height="90" alt="Trash bin"/>
-                        </svg>
-                        <img class="hide-png no-display svg-fallback delete-png" src="main/images/delete.png" alt="Trash bin">
-                        `);
-			bookList.tableBody.appendChild(item);
-			// counter++;
-		}
-	},
-
-};
-
-var bookModal = {
-	backdrop: document.getElementById("add-backdrop"),
-	close: document.getElementById("add-close"),
-	content: document.getElementById("add-content"),
-	title: document.getElementById("book-title-field"),
-	author: document.getElementById("book-author-field"),
-	pages: document.getElementById("book-pages-field"),
-	checkbox: document.getElementById("read-checkbox"),
-	submit: document.getElementById("book-submit")
 }
 
+// document objects
 
-// bookList.Book.prototype.toggleRead = function () {
-// 	this.read = !(this.read);
-// }
+// navigation bar objects
 
-for(let i = 0; i < bookList.readIcons.length; i++) {
-	bookList.readIcons[i].addEventListener("click", function(){
-		if (bookList.readIcons[i].classList.contains("read")) {
-			// Make it look like it's not read
-			bookList.readIcons[i].classList.remove("read");
-			bookList.readIcons[i].classList.add("not-read");
-			bookList.readIcons[i].setAttribute("src", "main/images/x-icon.png");
-			bookList.readIcons[i].setAttribute("alt", "X icon");
-
-			// Actually change the property Of the object in the library
-			bookList.library[i].read = false;
-
-		}
-		else if (bookList.readIcons[i].classList.contains("not-read")) {
-
-			// Make it look like it's not read
-			bookList.readIcons[i].classList.remove("not-read");
-			bookList.readIcons[i].classList.add("read");
-			bookList.readIcons[i].setAttribute("src", "main/images/check.png");
-			bookList.readIcons[i].setAttribute("alt", "check icon");
-
-			// Actually change the property Of the object in the library			
-			bookList.library[i].read = true;
-		}
-	});
+var navbar = {
+	header: document.querySelector(".main-header"),
+	name: document.querySelector("#nav__name"),
+	menubutton: document.querySelector(".nav__menu"),
+	menuitemscontainer: document.querySelector(".nav__items-container"),
+	menuitems: document.querySelector(".nav__items"),
+	menuitem: document.querySelectorAll(".nav__item"),
+	indicator: document.querySelectorAll(".indicator"),
+	showMenu: function showMenu() {
+		navbar.menuitemscontainer.classList.add("show")
+		navbar.menubutton.setAttribute("aria-expanded", "true");
+	},
+	hideMenu: function hideMenu() {
+		navbar.menuitemscontainer.classList.remove("show")
+		navbar.menubutton.setAttribute("aria-expanded", "false");
+	},
+	menutouched: false
 }
 
-
-
-
-
-
-bookList.addButton.addEventListener("click", function(){
-	helper.openModal(bookModal.backdrop);
-});
-
-bookModal.close.addEventListener("click", function(){
-	helper.closeModal(bookModal.backdrop);
-});
-
-bookModal.backdrop.addEventListener("click", function(event){
-	if(!((event.target === bookModal.content) || (bookModal.content.contains(event.target)))) {
-		helper.closeModal(bookModal.backdrop);
-
+navbar.menubutton.addEventListener("touchstart", function(){
+	if(navbar.menuitemscontainer.classList.contains("show")) {
+		navbar.hideMenu();
+		navbar.menutouched = true;
+	}
+	else if(!(navbar.menuitemscontainer.classList.contains("show"))) {
+		navbar.showMenu();
+		navbar.menutouched = true;
 	}
 });
 
+navbar.menubutton.addEventListener("click", function(){
+	if(navbar.menuitemscontainer.classList.contains("show") && (navbar.menutouched === false)) {
+		navbar.hideMenu();
+	}
+	else if(!(navbar.menuitemscontainer.classList.contains("show")) && (navbar.menutouched === false)) {
+		navbar.showMenu();
+	}
+	navbar.menutouched = false;
+});
 
-// delete an item from the book list
-for(let i = 0; i < bookList.deleteIcons.length; i++) {
-	bookList.deleteIcons[i].addEventListener("click", function(){
-		bookList.tableBody.removeChild(bookList.tableRows[i]);
+var about = {
+	top: document.querySelector("#about-section")
+}	
+
+var experience = {
+	top: document.querySelector("#experience-section")
+}
+
+var education = {
+	top: document.querySelector("#education-section")
+}
+
+var skills = {
+	top: document.querySelector("#skills-section")
+}
+
+var interests = {
+	top: document.querySelector("#interests-section")
+}
+
+var awards = {
+	top: document.querySelector("#awards-section")
+}
+
+// Get the offset top of the section elements
+var offset = { 
+	
+	aboutY: about.top.offsetTop - 30,
+	experienceY: experience.top.offsetTop - 30,
+	educationY: education.top.offsetTop - 30,
+	skillsY: skills.top.offsetTop - 30,
+	interestsY: interests.top.offsetTop - 30,
+	awardsY: awards.top.offsetTop - 30
+ }; 
+// Re-updates the offset upon resizing
+window.onresize = function (event) {
+
+	aboutY = about.top.offsetTop - 30;
+	experienceY = experience.top.offsetTop - 30;
+	educationY = education.top.offsetTop - 30;
+	skillsY = skills.top.offsetTop - 30; 
+	interestsY = interests.top.offsetTop - 30;
+	awardsY = awards.top.offsetTop - 30;
+
+	
+};
+
+// This checks how much the window is scrolled
+window.onscroll = function (event)
+{
+	
+	// Highlight about after scrolling to its position
+	if(this.scrollY < offset.experienceY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 0) {
+				navbar.menuitem[index].classList.remove("current");
+				navbar.indicator[index].classList.remove("show");
+			}
+
+		}
+		navbar.menuitem[0].classList.add("current");
+		navbar.indicator[0].classList.add("show");
+
+
+	}
+	// Highlight experience after scrolling down to its position
+	else if(this.scrollY > offset.experienceY && this.scrollY < offset.educationY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 1){
+
+			 navbar.menuitem[index].classList.remove("current");
+			 navbar.indicator[index].classList.remove("show");
+
+			}
+		}
+		navbar.menuitem[1].classList.add("current");
+		navbar.indicator[1].classList.add("show");
+	}
+
+	// Highlight education after scrolling down to its position
+	else if(this.scrollY > offset.educationY && this.scrollY < offset.skillsY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 2) {
+				navbar.menuitem[index].classList.remove("current");
+				navbar.indicator[index].classList.remove("show");
+			}
+		}
+		navbar.menuitem[2].classList.add("current");
+		navbar.indicator[2].classList.add("show");
+	}
+
+	// Highlight skills after scrolling down to its position
+	else if(this.scrollY > offset.skillsY && this.scrollY < offset.interestsY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 3) {
+				navbar.menuitem[index].classList.remove("current");
+				navbar.indicator[index].classList.remove("show");
+			}
+		}
+		navbar.menuitem[3].classList.add("current");
+		navbar.indicator[3].classList.add("show");
+	}
+
+	// Highlight interests after scrolling down to its position
+	else if(this.scrollY > offset.interestsY && this.scrollY < offset.awardsY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 4){
+
+			 	navbar.menuitem[index].classList.remove("current");
+			 	navbar.indicator[index].classList.remove("show");
+			}
+		}
+		navbar.menuitem[4].classList.add("current");
+		navbar.indicator[4].classList.add("show");
+	}
+
+	// highlight awards after scrolling to its position
+	else if(this.scrollY > offset.awardsY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 5){
+				navbar.menuitem[index].classList.remove("current");
+				navbar.indicator[index].classList.remove("show");
+			}
+		}
+		navbar.menuitem[5].classList.add("current");
+		navbar.indicator[5].classList.add("show");
+	}
+}
+
+
+for(let index = 0; index < navbar.menuitem.length; index++) {
+	navbar.menuitem[index].addEventListener("focus", function(){
+		// navbar.indicator[index].classList.add("show");
+	});
+
+	navbar.menuitem[index].addEventListener("blur", function(){
+		// navbar.indicator[index].classList.remove("show");
 	});
 
 }
 
+// navbar.menuitem[5].addEventListener("click", function(event){
+	// if (!(navbar.indicator[5].classList.contains("show"))) { }
+	// let distanceY = offset.awardsY - (window.scrollY);
+	// event.preventDefault();
+	// window.scrollBy(0, (0.0001 * distanceY));
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.0002 * distanceY));}, 30)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.0004 * distanceY));}, 60)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.0008 * distanceY));}, 90)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.0016 * distanceY));}, 120)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.0032 * distanceY));}, 150)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.0128 * distanceY));}, 210)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.0512 * distanceY));}, 270)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.2000 * distanceY));}, 330)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.5000 * distanceY));}, 360)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (0.55 * distanceY));}, 420)
+	// setTimeout(function(){let distanceY = offset.awardsY - (window.scrollY); window.scrollBy(0, (1 * distanceY));}, 480)})
 
-// Submit listener for the modal form
-bookModal.submit.addEventListener("submit", function(){
-	bookList.addBook(bookModal.title.value, bookModal.author.value, bookModal.pages.value, bookModal.checkbox.checked);	
-});
+
+
+
+
+
+
+
+
